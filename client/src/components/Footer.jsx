d231057e67b02611bb73fae7709ac6d1089e2092
Hari-Veera <medipudihariveera@gmail.com>
2025-08-03 21:10:39 +0530

    Minor changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,9 +7,6 @@ const Footer = () => {
             <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-14">
                 <div className="md:max-w-96">
                     <img className="w-36 h-auto" src={assets.logo} alt="logo" />
-                    <p className="mt-6 text-sm">
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </p>
                     <div className="flex items-center gap-2 mt-4">
                         <img src={assets.googlePlay} alt="google play" className="h-9 w-auto" />
                         <img src={assets.appStore} alt="app store" className="h-9 w-auto" />
@@ -35,7 +32,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="pt-4 text-center text-sm pb-5">
-                Copyright {new Date().getFullYear()} © GreatStack. All Right Reserved.
+                Copyright {new Date().getFullYear()} © M Hari Veera. All Right Reserved.
             </p>
         </footer>
   )
